15432935fcd6f074d46479ec706579c3ce55df1c
Armi64bit <bahaeddine.bouzid@esprit.tn>
2023-11-29 21:25:44 +0100

    Update ClubServiceApplication.java

--- club-service/src/main/java/com/example/clubservice/ClubServiceApplication.java
+++ club-service/src/main/java/com/example/clubservice/ClubServiceApplication.java
@@ -15,17 +15,5 @@ public class ClubServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ClubServiceApplication.class, args);
     }
-@Autowired
-	private CandidatRepository repository;
 
-
-	@Bean
-	ApplicationRunner init() {
-		return (args) -> {
-			repository.save(new Candidat("mariem","ma","ma@esprit.tn"));
-			repository.save(new Candidat("sarra","sa","sa@esprit.tn"));
-			repository.save(new Candidat("mohamed","mo","mo@esprit.tn"));
-			repository.findAll().forEach(System.out::println);
-		};
-	}
 }
